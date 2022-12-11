56364337bd510c42779d0f57b572a3cff53fd995
Nicolás Reyes Arias <nicolas.ra3333gmail.com>
2022-12-11 14:10:04 -0300

    primera maqueta

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './Footer';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+    <Header></Header>
+    <main>
+      <section className="servicios">
+        <div className="">
+          <h1>Servicio 1</h1>
+        </div>
+      </section>
+      <section className="noticias"></section>
+    </main>
+    <Footer></Footer>
+    </>
+
+    
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,24 @@
+import './App.css';
+function Footer() {
+  return (
+    <footer>
+      <div className="logo">
+        <img src="imagenes\Logo_Nido_Thani.png" alt='Logo'></img>
+      </div>
+      <div className="redes">
+        <h1>Redes Sociales</h1>
+        <ul>
+            <li>
+                <a href="facebook.com">Facebook</a>
+            </li>
+        </ul>
+      </div>
+      <div className="mapa">
+
+      </div>
+      
+    </footer>
+  );
+}
+
+export default Footer;
